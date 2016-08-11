ff89b4cfa8eb9b3d70c3691197f379966d635de3
Adrian Roman <aroman_ro@yahoo.com>
2016-08-11 23:26:34 +0300

    Removed misleading comment about 'noEvent'

--- MolecularDynamics/Event.h
+++ MolecularDynamics/Event.h
@@ -14,9 +14,7 @@ public:
 		// here other kind of events could be added - of course they must be handled appropriately
 		// for example crossings into other sectors
 
-		noEvent // this is not used in the program, at least not yet
-		// it could be used in case of using a priority queue (typically implemented with a heap) for events
-		// to 'fake' removal of events from the queue - just set their type to 'noEvent' and ignore such events when they are scheduled
+		noEvent
 	} EventType;
 
 	EventType type;
