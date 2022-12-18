ecd7ac6be33f4a976613fac80a523c66bc09eace
Adrian Roman <aroman_ro@yahoo.com>
2022-12-18 09:34:44 +0200

    Added tessellation shaders. They are not used, but I might use them in some future project.

--- MolecularDynamics/Shader.h
+++ MolecularDynamics/Shader.h
@@ -28,17 +28,15 @@ namespace OpenGL {
 		void setSource(const char* src);
 	};
 
-
-	class VertexShader : public Shader {
+	class TessellationControlShader : public Shader {
 	public:
-		VertexShader();
+		TessellationControlShader();
 		GLuint getType() const override;
 	};
 
-
-	class FragmentShader : public Shader {
+	class TessellationEvaluationShader : public Shader {
 	public:
-		FragmentShader();
+		TessellationEvaluationShader();
 		GLuint getType() const override;
 	};
 
@@ -53,5 +51,19 @@ namespace OpenGL {
 		ComputeShader();
 		GLuint getType() const override;
 	};
+
+	class VertexShader : public Shader {
+	public:
+		VertexShader();
+		GLuint getType() const override;
+	};
+
+
+	class FragmentShader : public Shader {
+	public:
+		FragmentShader();
+		GLuint getType() const override;
+	};
+
 }
 
